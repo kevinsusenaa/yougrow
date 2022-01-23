e758483e1239a4f786ee3e9f59dc417f3b9afdd8
kevinsusenaa <62592383+kevinsusenaa@users.noreply.github.com>
2022-01-23 18:53:45 +0700

    pushh

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'YouGro',
+      title: 'YouGrow',
       initialRoute: '/',
       routes: {
         '/': (context) => YGBottomBar(),
